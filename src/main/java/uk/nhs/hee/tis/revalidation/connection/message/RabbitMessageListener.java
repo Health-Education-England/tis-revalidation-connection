534018b246361ebf6ce8d951597e3e226c5c745b
jayanta2018 <jayanta.saha@hee.nhs.uk>
2021-02-19 12:16:58 +0000

    wip: added the code for saving data into es index

--- src/main/java/uk/nhs/hee/tis/revalidation/connection/message/RabbitMessageListener.java
+++ src/main/java/uk/nhs/hee/tis/revalidation/connection/message/RabbitMessageListener.java
@@ -5,14 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import uk.nhs.hee.tis.revalidation.connection.service.ElasticSearchService;
 
 
 @Slf4j
 @Component
 public class RabbitMessageListener {
 
-  //@Autowired
-  //private ESService;
+  @Autowired
+  private ElasticSearchService elasticSearchService;
 
   @RabbitListener(queues = "${app.rabbit.queue}")
   public void receivedMessage(final Object message) {
@@ -21,6 +22,7 @@ public class RabbitMessageListener {
     //Get ProgrammeMembershipType - Match if visitor  ||
     //Get ProgrammeMembershipEndDate - Match if expires
     //Push to Exception ES index
+    elasticSearchService.sendToElasticSearch();//We will have to pass the list of ExceptionView here
     //ALL OF THE ABOVE IN SERVICE
     //ESService.updateTrainee(gmcDoctor);
   }

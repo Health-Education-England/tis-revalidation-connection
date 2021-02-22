405c1060caa00030e0d3333a694c74d64ae09ecc
tisadmincw <tisadmincw@N2810.xhee.nhs.uk>
2021-02-22 10:15:53 +0000

    wip: added exception business logic and push to es

---
+++ src/main/java/uk/nhs/hee/tis/revalidation/connection/dto/ConnectionInfoDto.java
@@ -0,0 +1,2 @@
+package uk.nhs.hee.tis.revalidation.connection.dto;public class ConnectionInfoDto {
+}
---
+++ src/main/java/uk/nhs/hee/tis/revalidation/connection/service/ElasticSearchIndexUpdateHelper.java
@@ -0,0 +1,10 @@
+package uk.nhs.hee.tis.revalidation.connection.service;
+
+import uk.nhs.hee.tis.revalidation.connection.dto.ConnectionInfoDto;
+
+public class ElasticSearchIndexRouteHelper {
+
+  public void updateElasticSearchIndex(final ConnectionInfoDto connectionInfo) {
+    
+  }
+}

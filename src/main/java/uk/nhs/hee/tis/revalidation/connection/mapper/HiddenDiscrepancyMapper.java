e73f250936addd34f490caa15314940d19b4d759
NM2.7JBY9KP8 <cai.willis@nhs.net>
2026-03-06 13:10:49 +0000

    wip: propagate hidden discrepancies to elasticsearch

---
+++ src/main/java/uk/nhs/hee/tis/revalidation/connection/mapper/HiddenDiscrepancyMapper.java
@@ -0,0 +1,52 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright 2026 Crown Copyright (NHS England)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package uk.nhs.hee.tis.revalidation.connection.mapper;
+
+import java.time.LocalDateTime;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import uk.nhs.hee.tis.revalidation.connection.dto.HideDiscrepancyDto;
+import uk.nhs.hee.tis.revalidation.connection.entity.HiddenDiscrepancy;
+
+/**
+ * Mapper interface for converting HideDiscrepancyDto objects to HiddenDiscrepancy entities.
+ * This mapper uses MapStruct to automatically generate the implementation code for the mapping.
+ */
+@Mapper(componentModel = "spring")
+public interface HiddenDiscrepancyMapper {
+
+  /**
+   * Maps a HideDiscrepancyDto to a HiddenDiscrepancy entity.
+   *
+   * @param dto the HideDiscrepancyDto containing the data to be mapped
+   * @param gmcId the GMC ID of the doctor for whom the discrepancy is being hidden
+   * @param batchTime the timestamp when the hiding action is performed
+   * @return a HiddenDiscrepancy entity populated with data from the DTO and additional parameters
+   */
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "gmcReferenceNumber", source = "gmcId")
+  @Mapping(target = "hiddenForDesignatedBodyCode", source = "dto.hiddenForDesignatedBodyCode")
+  @Mapping(target = "hiddenBy", source = "dto.hiddenBy")
+  @Mapping(target = "reason", source = "dto.reason")
+  @Mapping(target = "hiddenDateTime", source = "batchTime")
+  HiddenDiscrepancy toEntity(HideDiscrepancyDto dto, String gmcId, LocalDateTime batchTime);
+}
---
+++ src/main/java/uk/nhs/hee/tis/revalidation/connection/repository/HiddenDiscrepancyRepository.java
@@ -0,0 +1,48 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright 2026 Crown Copyright (NHS England)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package uk.nhs.hee.tis.revalidation.connection.repository;
+
+import java.util.List;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import uk.nhs.hee.tis.revalidation.connection.entity.HiddenDiscrepancy;
+
+/**
+ * Repository interface for managing HiddenDiscrepancy entities in MongoDB.
+ */
+@Repository
+public interface HiddenDiscrepancyRepository extends MongoRepository<HiddenDiscrepancy, ObjectId> {
+
+  /**
+   * Finds hidden discrepancies by GMC reference numbers and designated body code.
+   *
+   * @param gmcReferenceNumbers         the list of GMC reference numbers to search for
+   * @param hiddenForDesignatedBodyCode the designated body code for which the discrepancies are
+   *                                    hidden
+   * @return a list of HiddenDiscrepancy entities matching the criteria
+   */
+  List<HiddenDiscrepancy> findByGmcReferenceNumberInAndHiddenForDesignatedBodyCode(
+      List<String> gmcReferenceNumbers,
+      String hiddenForDesignatedBodyCode
+  );
+}

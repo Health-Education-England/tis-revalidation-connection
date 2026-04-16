77c5c63188e02ffb6d37b89c7b78c7aaec1b19f5
NM2.7JBY9KP8 <cai.willis@nhs.net>
2026-04-16 11:46:36 +0100

    chore: code smells
    TIS21-8289

--- src/main/java/uk/nhs/hee/tis/revalidation/connection/dto/ShowDiscrepancyResponseDto.java
+++ src/main/java/uk/nhs/hee/tis/revalidation/connection/dto/ShowDiscrepancyResponseDto.java
@@ -22,8 +22,6 @@
 package uk.nhs.hee.tis.revalidation.connection.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;

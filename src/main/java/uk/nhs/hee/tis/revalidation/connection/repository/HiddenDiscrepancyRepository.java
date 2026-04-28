bd2e608fd68ff57a93c388c59427350665fa992d
NM2.7JBY9KP8 <cai.willis@nhs.net>
2026-04-28 12:39:58 +0100

    chore: remove unused import
    TIS21-8287

--- src/main/java/uk/nhs/hee/tis/revalidation/connection/repository/HiddenDiscrepancyRepository.java
+++ src/main/java/uk/nhs/hee/tis/revalidation/connection/repository/HiddenDiscrepancyRepository.java
@@ -22,7 +22,6 @@
 package uk.nhs.hee.tis.revalidation.connection.repository;
 
 import java.util.List;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import uk.nhs.hee.tis.revalidation.connection.entity.HiddenDiscrepancy;

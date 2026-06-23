c90ea4d5dad46e4b0082e1b4cfb187d40d8450da
NM2.7JBY9KP8 <cai.willis@nhs.net>
2026-06-23 10:31:26 +0100

    chore: cleanup used code - remove classes related to disconnected, remove outdated elasticsearch json, remove old elasticsearch config
    TIS21-8754

--- src/main/java/uk/nhs/hee/tis/revalidation/connection/mapper/ConnectionInfoMapper.java
+++ src/main/java/uk/nhs/hee/tis/revalidation/connection/mapper/ConnectionInfoMapper.java
@@ -26,7 +26,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import uk.nhs.hee.tis.revalidation.connection.dto.ConnectionInfoDto;
 import uk.nhs.hee.tis.revalidation.connection.entity.CurrentConnectionsView;
-import uk.nhs.hee.tis.revalidation.connection.entity.DisconnectedView;
 import uk.nhs.hee.tis.revalidation.connection.entity.MasterDoctorView;
 
 @Mapper(componentModel = "spring")
@@ -40,14 +39,6 @@ public interface ConnectionInfoMapper {
 
   List<ConnectionInfoDto> currentConnectionsToConnectionInfoDtos(
       List<CurrentConnectionsView> currentConnectionsViews);
-
-  @Mapping(target = "programmeMembershipType", source = "membershipType")
-  @Mapping(target = "programmeMembershipStartDate", source = "membershipStartDate")
-  @Mapping(target = "programmeMembershipEndDate", source = "membershipEndDate")
-  @Mapping(target = "dataSource", ignore = true)
-  ConnectionInfoDto disconnectedToDto(DisconnectedView userType);
-
-  List<ConnectionInfoDto> disconnectedToDtos(List<DisconnectedView> userTypes);
 
   @Mapping(target = "programmeMembershipType", source = "membershipType")
   @Mapping(target = "programmeMembershipStartDate", source = "membershipStartDate")
